71fc23858cb296ee7ddc93276de33c0df5ea84fa
MiraeNassr <miraenassr@gmail.com>
2026-01-13 15:34:35 +0100

    Update and add const, @click button and add new login form

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,16 @@
 <script setup>
-import inloggen from './components/inloggen.vue'
+import { ref, onMounted } from 'vue';
+//import axios from 'axios';
 
-class btn-login {
-  constructor(parameters) {
-    addevenlistener('click', function() {
-      window.location.href = 'components/inloggen';
-    });
-  }
-}
+const categories = ref([]);
+const selectionPath = ref([]);
+const isLoginModalOpen = ref(false);
+const API_URL = "http://localhost/backend/get_categories.php";
+
+const toggleLoginModal = () => {
+  isLoginModalOpen.value = !isLoginModalOpen.value;
+};
+
 </script>
 
 <template>
@@ -18,11 +21,30 @@ class btn-login {
       <input type="text" placeholder="Search books..">  
     </div>
     <div class="user-buttons">
-      <button class="btn-login">Inloggen</button>
+      <button class="btn-login" @click="toggleLoginModal">Inloggen</button>
       <button class="btn-regester">Word lid</button>
     </div>
   </header>
-  
+  <main class="content">
+<div class=""></div>
+  </main>
+  <div v-if="isLoginModalOpen" class="modal-overlay" @click.self="toggleLoginModal">
+  <div class="modal-content">
+    <span class="close-btn" @click="toggleLoginModal">&times;</span>
+    <h2>Login to Your Account</h2>
+    <form @submit.prevent="handleLogin">
+      <div class="form-group">
+        <label>Email:</label>
+        <input type="email" placeholder="Enter your email" required />
+      </div>
+      <div class="form-group">
+        <label>Password:</label>
+        <input type="password" placeholder="Enter your password" required />
+      </div>
+      <button type="submit" class="btn-submit">Login Now</button>
+    </form>
+  </div>
+</div>
 
 </div>
 </template>
