08ca48313981ea9cc89e31b0fd625b3e6db0ffdb
MiraeNassr <miraenassr@gmail.com>
2026-01-13 14:33:01 +0100

    Add search bar,logo and logoin, register buttons.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,15 +3,20 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+<div class="app-home">
+  <header class="navbar">
+    <div class="logo">Forum Library</div>
+    <div class="seearch-bar">
+      <input type="text" placeholder="Search books..">  
+    </div>
+    <div class="user-buttons">
+      <button class="btn-login">Inloggen</button>
+      <button class="btn-regester">Word lid</button>
+    </div>
+  </header>
+  
+
+</div>
 </template>
 
 <style scoped>
