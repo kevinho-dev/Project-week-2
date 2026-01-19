fa444ca03564b8ee62184938e709b05eb25c816a
MiraeNassr <miraenassr@gmail.com>
2026-01-19 10:11:04 +0100

    Bulding register file in php

---
+++ backend/register.php
@@ -0,0 +1,9 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json");
+
+include 'db_config.php';
+
+$data = json_decode(file_get_contents("php://input"));
