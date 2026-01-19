318b883ecdd08d00e3e0aad3580fa8b0ac93a3da
MiraeNassr <miraenassr@gmail.com>
2026-01-19 10:57:34 +0100

    Bulding login file in php

---
+++ backend/login.php
@@ -0,0 +1,16 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json");
+
+include 'db_config.php';
+//read login credentials
+$data = json_decode(file_get_contents("php://input"));
+
+if(!empty($data->email) && !empty($data->password)){
+    $email = $conn->real_escape_string($data->email);
+    $password = $data->password;
+
+    
+}
