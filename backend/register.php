20ce630b8f4f0e265c9ac7d410819b0b8387adea
MiraeNassr <miraenassr@gmail.com>
2026-01-19 10:26:19 +0100

    Add  user signup logic in register.php

--- backend/register.php
+++ backend/register.php
@@ -7,3 +7,21 @@ header("Content-Type: application/json");
 include 'db_config.php';
 
 $data = json_decode(file_get_contents("php://input"));
+
+if (!empty($data->email) && !empty($data->$password) && !empty($data->username)){
+    $full_name = $conn->real_escape_string($data->username);
+    $email = $conn->real_escape_string($data->email);
+    $role = $conn->real_escape_string($data->$role);
+    $password_hash = password_hash($data->password, PASSWORD_DEFAULT);
+
+    $sql ="INSERT INTO USERS (full_name, email, password_hash, role) 
+    VALUES ('$full_name', '$email', '$password_hash', '$role')";
+
+    if ($conn->query($sql)){
+        echo json_encode(["message" => "Success"];
+    }else{
+        http_response_code(400);
+        echo json_encode(["error" => "Email already registered"]);
+    }
+}
+?>
