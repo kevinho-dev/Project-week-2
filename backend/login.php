6751813fc4188bf8081b1dc4caef1888692d0120
MiraeNassr <miraenassr@gmail.com>
2026-01-19 11:07:59 +0100

    Add login authentication logic in login.php

--- backend/login.php
+++ backend/login.php
@@ -12,5 +12,23 @@ if(!empty($data->email) && !empty($data->password)){
     $email = $conn->real_escape_string($data->email);
     $password = $data->password;
 
-    
+    $sql = "SELECT user_id, full_name, password_hash, role FROM users WHERE email = '$email";
+    $result = $conn->query($sql);
+
+    if($result && $result->num_rows > 0){
+        $user = $result->fetch_assoc();
+        if(password_verify($password, $user['password_hash'])){
+            echo json_encode([
+                "user_id" => $user['user_id'],
+                "username" => $user['full_name'],
+                "role" => $user['role']
+            ]);
+        }else {
+            http_response_code(400);
+            echo json_encode(["error" => "Invalid password"]);
+        }
+     } else {
+        http_response_code(404);
+        echo json_encode(["error" => "User not found"]);
+     }
 }
